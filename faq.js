b0e4c9e9631c639c64d86dcc779910143ed4d095
Sunny <77973332+sunny080@users.noreply.github.com>
2022-01-18 16:30:54 +0500

    Add files via upload

---
+++ faq.js
@@ -0,0 +1,62 @@
+function openNav() {
+    document.querySelector(".kop").style.display = "flex";
+  }
+  
+  function closeNav() {
+    document.querySelector(".kop").style.display = "none";
+  }
+
+
+
+  function openYav() {
+    document.querySelector(".op").style.display = "flex";
+  }
+  
+  function closeYav() {
+    document.querySelector(".op").style.display = "none";
+  }
+
+
+  function openTav() {
+    document.querySelector(".lov").style.display = "flex";
+  }
+  
+  function closeTav() {
+    document.querySelector(".lov").style.display = "none";
+  }
+
+
+
+  function openPav() {
+    document.querySelector(".po").style.display = "flex";
+  }
+  
+  function closePav() {
+    document.querySelector(".po").style.display = "none";
+  }
+
+
+
+
+
+  function openLav() {
+    document.querySelector(".poo").style.display = "flex";
+  }
+  
+  function closeLav() {
+    document.querySelector(".poo").style.display = "none";
+  }
+
+
+
+
+
+
+
+  function openRav() {
+    document.querySelector(".pro").style.display = "flex";
+  }
+  
+  function closeRav() {
+    document.querySelector(".pro").style.display = "none";
+  }
