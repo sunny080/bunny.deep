be24633e1da41704803869e6f22ddcad943413b3
Sunny <trajputtrana@gmail.com>
2022-02-24 15:29:41 +0500

    scroll animation fix and another miner fixes

---
+++ scroll.js
@@ -0,0 +1,524 @@
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.para1' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.cons' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.head1' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.head2' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.para2' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.para3' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.btn7' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.heading6' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.paragraph' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.cardss1' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.cardss2' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.cardss3' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.cardss4' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.cardss5' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.heading8' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.maps1' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.maps2' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.maps3' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.maps4' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.maps5' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.maps6' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.maps7' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.maps8' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.maps9' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.maps10' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.intro' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.in-tex' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.tip' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.btn5' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.heade2-text' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.heade3-text' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.faqs1' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.faqs2' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.faqs3' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.faqs4' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.faqs5' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.faqs6' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('active');
+
+    }else{
+        content.classList.remove('active')
+    }
+
+});
+
+
